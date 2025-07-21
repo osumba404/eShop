32d322cd438e950592f1f767ed7bfa44a4866203
Evans Osumba <osumbaevanzz@gmail.com>
2025-07-21 10:51:35 +0300

    updated ui

--- eKiatu/lib/home_page.dart
+++ eKiatu/lib/home_page.dart
@@ -3,11 +3,69 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final List<String> filters = const [
+    "All", "Addias", "Nike", "Bata", "Jordans"
+  ];  
+
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromRGBO(225, 225, 225, 1),
+                  ),
+                  borderRadius: BorderRadius.horizontal(
+                    left: Radius.circular(28),
+                    )
+                );
+
     return Scaffold(
-      body: Column(
-        
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text("eKiatu\nCollection", 
+                                style: TextStyle(
+                  fontWeight: FontWeight.bold, 
+                  fontSize: 25,
+                                ),
+                                ),
+                ),
+
+              const Expanded(child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search", 
+
+                prefixIcon: Icon(Icons.search),   
+                border: border,
+                enabledBorder:  border,          
+                ),
+              )
+              ),
+              ]
+            ),
+
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+              itemCount: filters.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Chip(
+                    label: Text(filter),
+                    ),
+                );
+              
+              }
+              ),
+            )
+          ]
+        ),
       )
       );
   }
--- eKiatu/lib/main.dart
+++ eKiatu/lib/main.dart
@@ -12,12 +12,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Shopping App",
-      home: HomePage(),
+      home: const HomePage(),
        debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: "Lato",
       colorScheme: ColorScheme.fromSeed(
-        seedColor: Color.fromRGBO(254, 206, 1, 1))
+        seedColor: Color.fromRGBO(254, 206, 1, 1)),
+
+      inputDecorationTheme: const InputDecorationTheme(
+        hintStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+
+      prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+      ),
       ),
     );
   }
